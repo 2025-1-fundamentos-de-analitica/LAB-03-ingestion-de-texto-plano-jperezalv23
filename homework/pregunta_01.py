51bf19f30a6158876755f79bbcae4e881953a099
juanp <jperezalv@unal.edu.co>
2025-07-09 09:54:30 -0500

    solution

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -1,20 +1,42 @@
-"""
-Escriba el codigo que ejecute la accion solicitada en cada pregunta.
-"""
-
-# pylint: disable=import-outside-toplevel
-
+import pandas as pd
 
 def pregunta_01():
-    """
-    Construya y retorne un dataframe de Pandas a partir del archivo
-    'files/input/clusters_report.txt'. Los requierimientos son los siguientes:
+  
+  def parse_lines(lines):
+    rows = []
+    current_row = []
+    new_row = True
 
-    - El dataframe tiene la misma estructura que el archivo original.
-    - Los nombres de las columnas deben ser en minusculas, reemplazando los
-      espacios por guiones bajos.
-    - Las palabras clave deben estar separadas por coma y con un solo
-      espacio entre palabra y palabra.
+    for line in lines:
+      line = line.strip()
+      tokens = line.split()
 
+      if tokens and new_row:
+        current_row = [
+          int(tokens[0]),
+          int(tokens[1]),
+          float(tokens[2].replace(',', '.')),
+          " ".join(tokens[4:]),
+        ]
+        new_row = False
+      elif tokens:
+        current_row[-1] += " " + " ".join(tokens)
+      else:
+        current_row[-1] = current_row[-1].replace('.', '')
+        rows.append(current_row)
+        current_row = []
+        new_row = True
+    return rows
 
-    """
+  with open("files/input/clusters_report.txt", 'r') as file:
+    raw_lines = file.readlines()
+
+  content_lines = raw_lines[4:]
+  rows = parse_lines(content_lines)
+  columns = [
+    'cluster',
+    'cantidad_de_palabras_clave',
+    'porcentaje_de_palabras_clave',
+    'principales_palabras_clave'
+  ]
+  return pd.DataFrame(rows, columns=columns)
